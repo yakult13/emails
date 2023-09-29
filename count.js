bcd5468b05932d5bf2722bbee9813cf4cf88ed16
yakult13 <80197821+yakult13@users.noreply.github.com>
2023-09-30 07:33:29 +0800

    Create count.js

---
+++ count.js
@@ -0,0 +1,13 @@
+// Select all elements with the attribute 'data-ad-status' set to 'unfilled'
+const unfilledElements = document.querySelectorAll('[data-ad-status="unfilled"]');
+
+// Get the count of unfilled elements
+const unfilledCount = unfilledElements.length;
+
+// Check if the count is greater than 1
+if (unfilledCount > 1) {
+  // Redirect to the '/ads-check' page
+  window.location.href = '/ads-check';
+} else {
+  console.log(`Number of elements with data-ad-status="unfilled": ${unfilledCount}`);
+}
